4a715b1f15a43c708bef2ee64be447c494119f19
Erbol Betenov <betenov.e98@gmail.com>
2022-11-05 14:40:34 +0600

    Добавил картинки к карточкам
    Вывел весь список на экран

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import burger from './assets/burger.png'
+import fries from './assets/fries.png'
+import hotDog from './assets/hot-dog.png'
+import coffee from './assets/coffee.png'
+import tea from './assets/tea.png'
+import cola from './assets/cola.png'
+
 
 function App() {
+  interface Orders {
+    name:string;
+    price:number;
+    image: string
+  }
+  const Orders:Orders[] = [
+    {name:'Hamburger' , price:150, image:burger},
+    {name:'Fries' , price:100, image:fries},
+    {name:'Hot-dog' , price:110, image:hotDog},
+    {name:'Cofe' , price:80, image:coffee},
+    {name:'Tea' , price:20, image: tea},
+    {name:'Cola' , price:60, image:cola},
+  ]
+
+
+
+
+  const allOrders = Orders.map((order) =>
+    <div>
+        <div className="descr-orders">
+          <img alt='' src= {order.image}/>
+          <div>
+            <strong className="strong">{order.name.toUpperCase()}</strong>
+            <span>{order.price} KGS</span>
+          </div>
+        </div>
+    </div>
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <fieldset className='field'>
+        <legend>Детали заказа</legend>
+        <span>Добавьте заказ</span>
+      </fieldset>
+        <fieldset className='field-order'>
+          <legend> Меню заказа</legend>
+          {allOrders}
+        </fieldset>
     </div>
   );
 }
